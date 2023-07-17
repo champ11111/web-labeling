86eeaeb8c3e1044af8ed370560a35eb872822a00
Nattawat Kwansakul <champ_2543@hotmail.com>
2023-07-17 22:39:36 +0700

    feat(data): add user module

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,59 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Delete,
+  Body,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import { User, Prisma, Data } from '@prisma/client';
+
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getUsers(): Promise<User[]> {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User | null> {
+    return this.userService.getUserById(id);
+  }
+
+  @Get('/labelled-data/:username')
+  async getLabelledDataByUsername(
+    @Param('username') username: string,
+  ): Promise<Data[]> {
+    return this.userService.getLabelledDataByUsername(username);
+  }
+
+  @Get('/non-labelled-data/:username')
+  async getNonLabelledDataByUsername(
+    @Param('username') username: string,
+  ): Promise<Data[]> {
+    return this.userService.getNonLabelledDataByUsername(username);
+  }
+
+  @Post()
+  async createUser(@Body() data: Prisma.UserCreateInput): Promise<User> {
+    return this.userService.createUser(data);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: Prisma.UserUpdateInput,
+  ): Promise<User> {
+    return this.userService.updateUser(id, data);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return this.userService.deleteUser(id);
+  }
+}
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@nestjs/common';
+import { User, Prisma, Data } from '@prisma/client';
+
+import { PrismaService } from 'src/libs/prisma/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
+  }
+
+  async getUserById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { id } });
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({ data });
+  }
+
+  async updateUser(id: string, data: Prisma.UserUpdateInput): Promise<User> {
+    return this.prisma.user.update({ where: { id }, data });
+  }
+
+  async deleteUser(id: string): Promise<User> {
+    return this.prisma.user.delete({ where: { id } });
+  }
+
+  async getLabelledDataByUsername(username: string): Promise<Data[]> {
+    const users = await this.prisma.user.findUnique({
+      where: { username },
+      select: {
+        userData: {
+          where: { isLabelled: true },
+          select: {
+            data: true,
+          },
+        },
+      },
+    });
+    return users.userData.map((userData) => userData.data);
+  }
+
+  async getNonLabelledDataByUsername(username: string): Promise<Data[]> {
+    const users = await this.prisma.user.findUnique({
+      where: { username },
+      select: {
+        userData: {
+          where: { isLabelled: false },
+          select: {
+            data: true,
+          },
+        },
+      },
+    });
+    return users.userData.map((userData) => userData.data);
+  }
+}
